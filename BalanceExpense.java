09dfb745e23bf28346a0a810d28bb740e74b0d93
Thahirah <thorhyroar@gmail.com>
2024-03-08 02:00:10 +0800

    update
    testcase class improved
    in the midst of solving the main class

--- BalanceExpense.java
+++ BalanceExpense.java
@@ -17,7 +17,12 @@ public class BalanceExpense{
 
         for (Map.Entry<String,Double> entry : expenses.entrySet()){                    // calculate balance owed by each person
             String person = entry.getKey();                                            // input name
-            double amountPaid = entry.getValue();                                       // input amt paid by person
+            Double amountPaid = entry.getValue();                                       // input amt paid by person
+
+            if (amountPaid == null){
+                throw new IllegalArgumentException("Expense amount cannot be null.");
+            }
+
             double balance = amountPaid - averageExpense;                              // calculate balance by minus off average expense
             balances.put(person,balance);                                              // store balance in balance map
         }
@@ -27,33 +32,41 @@ public class BalanceExpense{
 
     public static List<String> settleBalances(Map<String, Double> balances){
         List<String> transactions = new ArrayList<>();                                // list to store transaction details
+        int numOfTransactions = 0;
 
         while (!balances.isEmpty()){                                                  // iterate untill all balance sttled
             String debtor = null;                                                     // person owes money
             String owedMoney = null;                                                  // person owed money
             double minDebt = Double.MAX_VALUE;                                        // min debt amt
-            double maxCredit = Double.MIN_VALUE;                                     // max credit amt
+            double maxOwed = Double.MIN_VALUE;                                     // max credit amt
 
             for (Map.Entry<String, Double> entry : balances.entrySet()){
                 String person =  entry.getKey();                                     // input person name
-                double balance = entry.getValue();                                   // get balancee
+                Double balance = entry.getValue();                                   // get balancee
 
-                if (balance < minDebt){                                              // update debtor & minDebt if balance<current minDebt
+                if (balance == null){
+                    throw new IllegalArgumentException("Balance amount cannot be null.");
+                }
+
+                double balanceValue = balance.doubleValue();
+
+                if (balanceValue < minDebt){                                              // update debtor & minDebt if balance<current minDebt
                     debtor = person;
                     minDebt = balance;
                 }
 
-                if (balance > maxCredit){                                            //update owedMoney and maxCredit if balance>current maxCredit
+                if (balanceValue > maxOwed){                                            //update owedMoney and maxCredit if balance>current maxCredit
                     owedMoney = person;
-                    maxCredit = balance;
+                    maxOwed = balance;
                 }
             }
-            double amount = Math.min(-minDebt, maxCredit);                           // calculate amt to transfer
+            double amount = Math.min(-minDebt, maxOwed);                           // calculate amt to transfer
 
             balances.put(owedMoney, balances.get(owedMoney) - amount);               // update balance aft transaction
             balances.put(debtor, balances.get(debtor) + amount);
 
             transactions.add(debtor + " pays " + owedMoney + "$" + String.format("%.2f", amount));    // add transaction details to list
+            numOfTransactions++;
 
             if (balances.get(debtor) == 0.0){                                        // remove ppl with 0 balance
                 balances.remove(debtor);
@@ -64,6 +77,7 @@ public class BalanceExpense{
             }
         }
 
+        transactions.add("Number of transactions: " +  numOfTransactions);
         return transactions;
     }
 }
--- BalanceExpenseTest.java
+++ BalanceExpenseTest.java
@@ -1,109 +1,6 @@
-// import org.junit.jupiter.api.Test;
-// import java.util.HashMap;
-// import java.util.Map;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// public class BalanceExpenseTest{
-
-//     @Test
-//     public void testpersonPay2ppl2dp(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Ali", 40.105);
-//         expenses.put("Bob", 40.105);
-//         expenses.put("Charlie", 10.00);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(10.03, balances.get("Ali"));
-//         assertEquals(10.04, balances.get("Bob"));
-//         assertEquals(-20.07, balances.get("Charlie"));
-//     }
-
-//     @Test
-//     public void test1personPay2ppl(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Ali", 40.0);
-//         expenses.put("Bob", 40.0);
-//         expenses.put("Charlie", 10.0);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(10.0, balances.get("Ali"));
-//         assertEquals(10.0, balances.get("Bob"));
-//         assertEquals(-20.0, balances.get("Charlie"));
-//     }
-
-
-//     @Test
-//     public void test4ppl(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Ali", 10.0);
-//         expenses.put("Bob", 20.0);
-//         expenses.put("Charlie", 0.0);
-//         expenses.put("Don", 10.0);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(0.0, balances.get("Ali"));
-//         assertEquals(10.0, balances.get("Bob"));
-//         assertEquals(-10.0, balances.get("Charlie"));
-//         assertEquals(0.0, balances.get("Don"));
-//     }
-
-//     @Test
-//     public void test4pplUnequalPay(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Alice", 40.0);
-//         expenses.put("Bob", 40.0);
-//         expenses.put("Charlie", 10.0);
-//         expenses.put("Don", 10.0);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(15.0, balances.get("Bob"));
-//         assertEquals(-15.0, balances.get("Don"));
-//         assertEquals(15.0, balances.get("Alice"));
-//         assertEquals(-15.0, balances.get("Charlie"));
-//     }
-
-//     @Test
-//     public void test4ppl1PayMore(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Alice", 200.0);
-//         expenses.put("Bob", 80.0);
-//         expenses.put("Charlie", 50.0);
-//         expenses.put("Don", 20.0);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(-7.50, balances.get("Bob"));
-//         assertEquals(-37.50, balances.get("Charlie"));
-//         assertEquals(-67.50, balances.get("Don"));
-//         assertEquals(112.50, balances.get("Alice"));
-//     }
-
-//     @Test
-//     public void test4ppl2PayMore(){
-//         Map<String, Double> expenses = new HashMap<>();
-//         expenses.put("Alice", 160.0);
-//         expenses.put("Bob", 120.0);
-//         expenses.put("Charlie", 50.0);
-//         expenses.put("Don", 20.0);
-
-//         Map<String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-//         assertEquals(-67.50, balances.get("Don"));
-//         assertEquals(-37.30, balances.get("Charlie"));
-//         assertEquals(35.0, balances.get("Alice"));
-//         assertEquals(32.50, balances.get("Bob"));
-//     }
-
-// }
-
-
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class BalanceExpenseTest{
     public static void main(String[] args){
@@ -123,15 +20,17 @@ public class BalanceExpenseTest{
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
 
-        if (balances.get("Ali") == 10.03 && 
-        balances.get("Bob") == 10.04 && 
-        balances.get("Charlie") == -20.07){
-            System.out.println("Test for 1 person paying 2 people with 2 dp passed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
 
-        else{
-            System.out.println("Test for 1 person paying 2 people with 2 dp failed");
-        }
+        System.out.println();
+
+        // assert balances.get("Ali") == 10.03;
+        // assert balances.get("Bob") == 10.04;
+        // assert balances.get("Charlie") == -20.07;
+        
     }
 
     private static void test1personPay2ppl() {
@@ -141,16 +40,17 @@ public class BalanceExpenseTest{
         expenses.put("Charlie", 10.00);
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-        if (balances.get("Ali") == 10.0 && 
-        balances.get("Bob") == 10.0 && 
-        balances.get("Charlie") == -20.0){
-            System.out.println("Test for 1 person paying 2 people passed");
-        }
         
-        else{
-            System.out.println("Test for 1 person paying 2 people failed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
+
+        System.out.println();
+        // assert balances.get("Ali") == 10.0;
+        // assert balances.get("Bob") == 10.0;
+        // assert balances.get("Charlie") == -20.0;
+        
     }
 
     private static void test4ppl() {
@@ -161,17 +61,18 @@ public class BalanceExpenseTest{
         expenses.put("Don", 10.0);
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-        if (balances.get("Ali") == 0.0 && 
-        balances.get("Bob") == 10.0 && 
-        balances.get("Charlie") == -10.0 &&
-        balances.get("Don") == 0.0){
-            System.out.println("Test for 4 people passed");
-        }
         
-        else{
-            System.out.println("Test for 4 people failed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
+
+        System.out.println();
+        // assert balances.get("Ali") == 0.0;
+        // assert balances.get("Bob") == 10.0;
+        // assert balances.get("Charlie") == -10.0;
+        // assert balances.get("Don") == 0.0;
+        
     }
 
     private static void test4pplUnequalPay() {
@@ -182,17 +83,18 @@ public class BalanceExpenseTest{
         expenses.put("Don", 10.0);
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-        if (balances.get("Alice") == 15.0 && 
-        balances.get("Bob") == 15.0 && 
-        balances.get("Charlie") == -15.0 &&
-        balances.get("Don") == -15.0){
-            System.out.println("Test for 4 people with unequal pay passed");
-        }
         
-        else{
-            System.out.println("Test for 4 people with unequal pay failed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
+
+        System.out.println();
+        // assert balances.get("Alice") == 15.0;
+        // assert balances.get("Bob") == 15.0;
+        // assert balances.get("Charlie") == -15.0;
+        // assert balances.get("Don") == -15.0;
+        
     }
 
     private static void test4ppl1PayMore() {
@@ -203,17 +105,18 @@ public class BalanceExpenseTest{
         expenses.put("Don", 20.0);
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-        if (balances.get("Alice") == 112.50 && 
-        balances.get("Bob") == -7.50 && 
-        balances.get("Charlie") == -37.50 &&
-        balances.get("Don") == -67.50){
-            System.out.println("Test for 4 people with 1 person paying more passed");
-        }
         
-        else{
-            System.out.println("Test for 4 people with 1 person paying more failed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
+
+        System.out.println();
+        // assert balances.get("Alice") == 112.50;
+        // assert balances.get("Bob") == -7.50;
+        // assert balances.get("Charlie") == -37.50;
+        // assert balances.get("Don") == -67.50;
+
     }
 
     private static void test4ppl2PayMore() {
@@ -224,17 +127,17 @@ public class BalanceExpenseTest{
         expenses.put("Don", 20.0);
 
         Map <String, Double> balances = BalanceExpense.balanceExpenses(expenses);
-
-        if (balances.get("Alice") == 35.0 && 
-        balances.get("Bob") == 32.50 && 
-        balances.get("Charlie") == -37.30 &&
-        balances.get("Don") == -67.50){
-            System.out.println("Test for 4 people with 1 person paying more passed");
-        }
         
-        else{
-            System.out.println("Test for 4 people with 1 person paying more failed");
+        List <String> transactions = BalanceExpense.settleBalances(balances);
+        for (String transaction : transactions){
+            System.out.println(transaction);
         }
+
+        System.out.println();
+        // assert balances.get("Alice") == 35.0;
+        // assert balances.get("Bob") == 32.50;
+        // assert balances.get("Charlie") == -37.30;
+        // assert balances.get("Don") == -67.50;
     }
     
 }
