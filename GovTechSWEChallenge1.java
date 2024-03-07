3248b58092f0e9caa41b095eac90d3bf35892076
Thahirah <thorhyroar@gmail.com>
2024-03-07 17:51:10 +0800

    update main code

--- GovTechSWEChallenge1.java
+++ GovTechSWEChallenge1.java
@@ -6,23 +6,48 @@ import java.util.Map;
 public class BalanceExpense{
 
     public static Map<String,Double> balanceExpenses(Map<String,Double> expenses){
-        double totalExpense = 0;
-        Map<String, Double> balances = new HashMap<>;
+        double totalExpense = 0;                                                       // variable to store total expenses by everyone
+        Map<String, Double> balances = new HashMap<>;                                  // map to store balance owed by each person
 
-        for(double expense : expenses.values()){
+        for(double expense : expenses.values()){                                       // calculate total expense by everyone
             totalExpense += expense;
         }
 
-        double averageExpense = totalExpense / expenses.size();
+        double averageExpense = totalExpense / expenses.size();                        // averagee expense/person
 
-        for (Map.Entry<String,Double> entry : expenses.entrySet()){
-            String person = entry.getKey();
-            double amontPaid = entry.getValue();
-            double balance = amountPaid - averageExpense;
-            balances.put(person,balance);
+        for (Map.Entry<String,Double> entry : expenses.entrySet()){                    // calculate balance owed by each person
+            String person = entry.getKey();                                            // input name
+            double amontPaid = entry.getValue();                                       // input amt paid by person
+            double balance = amountPaid - averageExpense;                              // calculate balance by minus off average expense
+            balances.put(person,balance);                                              // store balance in balance map
         }
 
         return balances;
     }
-    
+
+    public static List<String> settleBalances(Map<String, Double> balances){
+        List<String> transactions = new ArrayList<>();                                // list to store transaction details
+
+        while (!balances.isEmpty()){                                                  // iterate untill all balance sttled
+            String debtor = null;                                                     // person owes money
+            String owedMoney = null;                                                  // person owed money
+            double minDebt = Double.MAX_VALUE;                                        // min debt amt
+            double maxCredit = Double.MIN_VALUE;                                     // max credit amt
+
+            for (Map.Entry<String, Double> entry : balances.entrySet()){
+                String person =  entry.getKey();                                     // input person name
+                double balance = entry.getValue();                                   // get balancee
+
+                if (balance < minDebt){                                              // update debtor & minDebt if balance<current minDebt
+                    debtor = person;
+                    minDebt = balance;
+                }
+
+                if (balance > maxCredit){                                            //update owedMoney and maxCredit if balance>current maxCredit
+                    owedMoney = person;
+                    maxCredit = balance;
+                }
+            }
+        }
+    }
 }
